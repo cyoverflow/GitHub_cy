016512fc6d6507e4ada087b4d117c998c5f9a405
Ckddn <Ckddn0925@163.com>
2022-12-21 18:26:45 +0800

    221221C_note

--- CLearning/learning-test/test1.c
+++ CLearning/learning-test/test1.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int a[] = {1,2,3,3,4};
-    int *p;
-    p = a;
-    printf("%d\n",p == &a[0]);
-    printf("%d\n",*p == a[0]);
-    printf("%d\n",p[0] == a[0]);
+    char str[] = "Are we having fun yet?";
 
+    printf("Str is :%s\n",str);
+    //puts(str);
 }
