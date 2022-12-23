04c113d008c157795f15945656c44d1a3b5f46f7
Ckddn <Ckddn0925@163.com>
2022-12-23 21:03:21 +0800

    221223C_note

--- CLearning/learning-test/test1.c
+++ CLearning/learning-test/test1.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    char str[] = "Are we having fun yet?";
-
-    printf("Str is :%s\n",str);
-    //puts(str);
+        printf("File :%s\n", __FILE__ );
+        printf("Date :%s\n", __DATE__ );
+        printf("Time :%s\n", __TIME__ );
+        printf("Line :%d\n", __LINE__ );
+        printf("ANSI :%d\n", __STDC__ );
 }
