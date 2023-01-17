1ee98d2ee7bec8c5bd27645ece048f5625e42a7d
Ckddn <Ckddn0925@163.com>
2023-01-18 01:35:47 +0800

    230118C_learning and new note for tips in C

--- CLearning/learning-test/test1.c
+++ CLearning/learning-test/test1.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 
-#define SQR(x) ((x) * (x))
+typedef struct complex{
+    double re, im;
+} Complex;
 
+Complex make_complex(double, double);
+Complex add_complex(Complex *c1,Complex *c2);
 
 int main() {
-#undef SQR
+    Complex c1 = {1.0, 0.0};
+    Complex c2 = {1.0, 0.0};
+    Complex c3 = {1.0, 0.0};
 
-#define SQR
-    printf("%d", SQR(5));
+    Complex c4 = make_complex(1.1, 2.0);
+
+    Complex c5 = add_complex(&c1, &c2);
+    printf("%.2lf\n%.2lf", c5.re,c5.im);
+}
+
+Complex add_complex(Complex *c1,Complex *c2) {
+    Complex c5 = {c1->re + c2->re, c1->im + c2->im};
+    return c5;
+}
+
+Complex make_complex(double re, double im) {
+    Complex com = {re, im};
+    return com;
 }
